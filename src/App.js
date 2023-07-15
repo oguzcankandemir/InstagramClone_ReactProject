22fa861d863f218a2dc2fa65a440752b745f214d
Oğuzcan Kandemir <90668203+oguzcankandemir@users.noreply.github.com>
2023-07-15 15:26:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App(){
+    return(
+        <div className="h-full w-full flex items-center justify-center">
+        <div className="w-[380px] h-[581px] bg-red-300">
+        <img className="w-[400px] h-[581px]"src="/images/instagram/xig/homepage/screenshots/screenshot1.png?__d=www"/>
+        </div>
+        </div>
+      
+       
+       
+    );
+}
+
+export default App;
