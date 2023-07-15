cbfc37b2a869ef331c191a1c45617f3a78a6554a
Oğuzcan Kandemir <90668203+oguzcankandemir@users.noreply.github.com>
2023-07-15 20:35:58 +0300

    Create Components and Building Navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-function App(){
-    return(
-        <div className="h-full w-full flex items-center justify-center">
-        <div className="w-[380px] h-[581px] bg-red-300">
-        <img className="w-[400px] h-[581px]"src="/images/instagram/xig/homepage/screenshots/screenshot1.png?__d=www"/>
-        </div>
-        </div>
-      
-       
-       
-    );
+import "./App.css";
+import Homepage from "./Homepage";
+
+function App() {
+  return (
+  <div className="app">
+    <Homepage />
+  </div>
+  );
 }
 
 export default App;
---
+++ src/timeline/Suggestions.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Suggestions.css";
+function Suggestions() {
+  return (
+    <div>Suggestions</div>
+  )
+}
+
+export default Suggestions
